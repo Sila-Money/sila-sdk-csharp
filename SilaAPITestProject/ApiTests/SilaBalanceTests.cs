eef177e0f28bbf3d7df0028258c8de7f894c2fb4
Karlo Roberto Alejandro Lorenzana Ramos <karlo@catalystdevs.com>
2019-10-25 13:19:00 -0600

    MOD: port modified in silabalance

--- SilaAPITestProject/ApiTests/SilaBalanceTests.cs
+++ SilaAPITestProject/ApiTests/SilaBalanceTests.cs
@@ -19,13 +19,13 @@ namespace SilaApiTest
         private void createWebServer()
         {
             string[] prefixes = new string[1];
-            prefixes[0] = "http://localhost:1080/silaBalance/";
+            prefixes[0] = "http://localhost:1081/silaBalance/";
             WebServer.TestHttpServer.Listener(prefixes);
         }
         [TestMethod]
         public void Response200Success()
         {
-            ApiResponse<object> response = api.SilaBalance("http://localhost:1080", "0xabc123abc123abc123");
+            ApiResponse<object> response = api.SilaBalance("http://localhost:1081", "0xabc123abc123abc123");
 
             Assert.AreEqual(200, response.StatusCode);
         }
