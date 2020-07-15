efc43709aac5bdd8bee65c2f4da2756a062161b0
Karlo <klorenzana@digitalgeko.com>
2020-07-15 09:33:28 -0600

    Added GetNaicsCategories endpoint

--- SilaAPI/silamoney/client/api/SilaApi.cs
+++ SilaAPI/silamoney/client/api/SilaApi.cs
@@ -440,6 +440,23 @@ namespace SilaAPI.silamoney.client.api
             return MakeRequest<BusinessRolesResponse>(path, body);
         }
 
+        /// <summary>
+        /// Makes a call to /get_business_types.
+        /// </summary>
+        /// <returns>ApiResponse&lt;object&gt; object with the server response</returns>
+        public ApiResponse<object> GetNaicsCategories()
+        {
+            var path = "/get_naics_categories";
+            Dictionary<String, String> header = new Dictionary<string, string>();
+            header.Add("created", EpochUtils.getEpoch().ToString());
+            header.Add("auth_handle", Configuration.AppHandle);
+
+            Dictionary<String, object> body = new Dictionary<string, object>();
+            body.Add("header", header);
+
+            return MakeRequest<NaicsCategoriesResponse>(path, body);
+        }
+
         private ApiResponse<object> MakeRequest<T>(string path, object body, string userPrivateKey = null)
         {
             var headerParams = new Dictionary<string, string>();
