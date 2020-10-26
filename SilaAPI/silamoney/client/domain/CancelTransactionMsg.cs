919faafd11f358e6a765e2e4a9ea6030527479e5
Jose Morales <jlmruiz@outlook.com>
2020-10-26 17:17:07 -0600

    Added support for /cancel_transaction. Added test case for /cancel_transaction.

---
+++ SilaAPI/silamoney/client/domain/CancelTransactionMsg.cs
@@ -0,0 +1,35 @@
+using System.Runtime.Serialization;
+
+namespace SilaAPI.silamoney.client.domain
+{
+    /// <summary>
+    /// Object used in the /cancel_transaction endpoint
+    /// </summary>
+    [DataContract]
+    public class CancelTransactionMsg
+    {
+        /// <summary>
+        /// The header object
+        /// </summary>
+        [DataMember(Name = "header", EmitDefaultValue = false)]
+        public Header Header { get; set; }
+
+        /// <summary>
+        /// The transaction id to be cancel
+        /// </summary>
+        [DataMember(Name = "transaction_id", EmitDefaultValue = false)]
+        public string TransactionId { get; set; }
+
+        /// <sumary>
+        /// The cancel transaction message constructor
+        /// <param name="authHandle">The application handle</param>
+        /// <param name="userHandle">The user handle</param>
+        /// <param name="transactionId">The transaction id</param>
+        /// </sumary>
+        public CancelTransactionMsg(string authHandle, string userHandle, string transactionId)
+        {
+            this.Header = new Header(userHandle, authHandle);
+            this.TransactionId = transactionId;
+        }
+    }
+}
