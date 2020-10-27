13e6dc0488a0794222afe786bca539f1a7a685c9
José Luis Morales Ruiz <jlmruiz@outlook.com>
2020-10-26 22:03:54 -0600

    Added support for /documents. Added test cases for /documents. Added documentation for /documents.

--- SilaAPI/silamoney/client/security/ECDSAUtil.cs
+++ SilaAPI/silamoney/client/security/ECDSAUtil.cs
@@ -1,4 +1,7 @@
 ﻿using Nethereum.Signer;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SilaAPI.silamoney.client.security
 {
@@ -20,6 +23,23 @@ namespace SilaAPI.silamoney.client.security
             string sig = messageSigner.HashAndSign(message, privateKey);
 
             return sig.Substring(2);
+        }
+
+        /// <summary>
+        /// Hashes a file with SHA256 algorithm
+        /// </summary>
+        /// <param name="file">The filestream of the file</param>
+        /// <returns></returns>
+        public static string HashFile(FileStream file)
+        {
+            var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(file);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
         }
     }
 }
