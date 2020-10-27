11538cb7a72309dba14dff6aaa1b192db1858f12
Jose Morales <jlmruiz@outlook.com>
2020-10-27 11:18:04 -0600

    Fixed SonarQube warnings

--- SilaAPI/silamoney/client/domain/EmailData.cs
+++ SilaAPI/silamoney/client/domain/EmailData.cs
@@ -2,8 +2,14 @@
 
 namespace SilaAPI.silamoney.client.domain
 {
+    /// <sumary>
+    ///
+    /// </sumary>
     public class EmailData : RegistrationDataBase
     {
+        /// <sumary>
+        ///
+        /// </sumary>
         [JsonProperty("email")]
         public string Email { get; set; }
     }
--- SilaAPI/silamoney/client/domain/EmailResponse.cs
+++ SilaAPI/silamoney/client/domain/EmailResponse.cs
@@ -2,8 +2,14 @@
 
 namespace SilaAPI.silamoney.client.domain
 {
+    /// <sumary>
+    ///
+    /// </sumary>
     public class EmailResponse : BaseResponseWithoutReference
     {
+        /// <sumary>
+        ///
+        /// </sumary>
         [JsonProperty("email")]
         public EmailData Email { get; set; }
     }
