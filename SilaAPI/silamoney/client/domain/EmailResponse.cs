e9ef41432e2c4a945b035e0a82f7d08ca127ad21
José Luis Morales Ruiz <jlmruiz@outlook.com>
2020-10-27 01:39:50 -0600

    Added support for /add/email. Added test case for for /add/email. Added documentation for /add/email.

---
+++ SilaAPI/silamoney/client/domain/EmailResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SilaAPI.silamoney.client.domain
+{
+    public class EmailResponse : BaseResponseWithoutReference
+    {
+        [JsonProperty("email")]
+        public EmailData Email { get; set; }
+    }
+}
---
+++ SilaAPITestProject/ApiTests/Test009AddRegistrationTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SilaAPI.silamoney.client.api;
+using SilaAPI.silamoney.client.domain;
+using System;
+
+namespace SilaApiTest
+{
+    [TestClass]
+    public class Test009AddRegistrationTest
+    {
+        SilaApi api = new SilaApi(DefaultConfig.environment, DefaultConfig.privateKey, DefaultConfig.appHandle);
+
+        [TestMethod("1 - AddAddress - Success Response")]
+        public void Response200Address()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod("2 - AddEmail - Success Response")]
+        public void Response200Email()
+        {
+            var user = DefaultConfig.FirstUser;
+            var email = "some.new.email@domain.go";
+            var response = api.AddEmail(user.UserHandle, user.PrivateKey, email);
+
+            Assert.AreEqual(200, response.StatusCode);
+            var parsedResponse = (EmailResponse)response.Data;
+            Assert.IsTrue(parsedResponse.Success);
+            Assert.AreEqual("SUCCESS", parsedResponse.Status);
+            Assert.IsTrue(parsedResponse.Message.Contains("Successfully added email"));
+            Assert.IsNotNull(parsedResponse.Email);
+            Assert.IsNotNull(parsedResponse.Email.AddedEpoch);
+            Assert.IsNotNull(parsedResponse.Email.ModifiedEpoch);
+            Assert.IsNotNull(parsedResponse.Email.Uuid);
+            Assert.AreEqual(email, parsedResponse.Email.Email);
+        }
+
+        [TestMethod("3 - AddIdentity - Success Response")]
+        public void Response200Identity()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod("4 - AddPhone - Success Response")]
+        public void Response200Phone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
