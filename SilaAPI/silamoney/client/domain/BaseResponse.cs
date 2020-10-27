63613e9cca7190eb1e3711fa5d705a2d11ddb7fe
José Luis Morales Ruiz <jlmruiz@outlook.com>
2020-10-27 00:41:11 -0600

    Added support for /delete/<registration-data>. Added test cases for /delete/<registration-data>. Added documentation for /delete/<registration-data>.

--- SilaAPI/silamoney/client/domain/BaseResponse.cs
+++ SilaAPI/silamoney/client/domain/BaseResponse.cs
@@ -1,32 +1,16 @@
 ﻿using Newtonsoft.Json;
-using System;
 
 namespace SilaAPI.silamoney.client.domain
 {
     /// <summary>
     /// Object used to store response message.
     /// </summary>
-    public class BaseResponse
+    public class BaseResponse : BaseResponseWithoutReference
     {
         /// <summary>
         /// String field used in the BaseResponse object to save reference
         /// </summary>
         [JsonProperty("reference")]
         public string Reference { get; set; }
-        /// <summary>
-        /// String field used in the BaseResponse object to save message 
-        /// </summary>
-        [JsonProperty("message")]
-        public string Message { get; set; }
-        /// <summary>
-        /// String field used in the BaseResponse object to save status
-        /// </summary>
-        [JsonProperty("status")]
-        public string Status { get; set; }
-        /// <summary>
-        /// Boolean field used to indicate if the response was successful or not
-        /// </summary>
-        [JsonProperty("success")]
-        public Boolean Success { get; set; }
     }
 }
---
+++ SilaAPI/silamoney/client/domain/RegistrationData.cs
@@ -0,0 +1,36 @@
+﻿namespace SilaAPI.silamoney.client.domain
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class RegistrationData
+    {
+        private readonly string url;
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly RegistrationData Email = new RegistrationData("email");
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly RegistrationData Phone = new RegistrationData("phone");
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly RegistrationData Identity = new RegistrationData("identity");
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly RegistrationData Address = new RegistrationData("address");
+
+        private RegistrationData(string url)
+        {
+            this.url = url;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Url { get { return url; } }
+    }
+}
