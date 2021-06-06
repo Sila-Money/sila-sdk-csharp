7228cd59137da622d7ce656cd1fd786bda1ff545
Karlo Lorenzana <klorenzana@digitalgeko.com>
2021-06-05 23:20:51 -0600

    Adding get institutions old way

---
+++ SilaAPITestProject/ApiTests/Test001GetInstitutions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SilaAPI.silamoney.client.api;
+using SilaAPI.silamoney.client.domain;
+
+namespace SilaApiTest
+{
+    [TestClass]
+    public class Test001GetInstitutions
+    {
+        SilaApi api = DefaultConfig.Client;
+
+        [TestMethod("1 - GetInstitutions - Successful get institutions")]
+        public void T001Response200()
+        {
+            InstitutionSearchFilters searchFilters = new InstitutionSearchFilters{
+                InstitutionName = "1st advantage bank"
+            };
+            var response = api.GetInstitutions(searchFilters);
+            var parsedResponse = (GetInstitutionsResponse)response.Data;
+
+            Assert.AreEqual(200, response.StatusCode);
+        }
+    }
+}
