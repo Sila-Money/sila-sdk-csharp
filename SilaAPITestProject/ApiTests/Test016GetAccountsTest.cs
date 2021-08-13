ecc598e39a8c7d81f1ef870757fe4f83d6ee6560
ram <rbahadur@arcgate.com>
2021-08-13 10:56:03 +0530

    Baseline bug fixes: linked accounts count updated

--- SilaAPITestProject/ApiTests/Test016GetAccountsTest.cs
+++ SilaAPITestProject/ApiTests/Test016GetAccountsTest.cs
@@ -18,7 +18,7 @@ namespace SilaApiTest
 
             Assert.AreEqual(200, response.StatusCode, $"{user.UserHandle} should success get_accounts");
             var parsedResponse = (GetAccountsResponse)response.Data;
-            Assert.AreEqual(6, parsedResponse.Accounts.Count, $"{user.UserHandle} must have 3 linked accounts");
+            Assert.AreEqual(5, parsedResponse.Accounts.Count, $"{user.UserHandle} must have 3 linked accounts");
         }
 
         [TestMethod("2 - GetAccounts - Empty user handle failure")]
