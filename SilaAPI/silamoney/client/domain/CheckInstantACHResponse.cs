44791f5aceb4680574cbdae27f9c973894927443
ram <rbahadur@arcgate.com>
2021-10-08 15:18:20 +0530

    Added MaximumAmount extra parameter to the response in /check_instant_ach

--- SilaAPI/silamoney/client/domain/CheckInstantACHResponse.cs
+++ SilaAPI/silamoney/client/domain/CheckInstantACHResponse.cs
@@ -1,7 +1,13 @@
+using Newtonsoft.Json;
 namespace SilaAPI.silamoney.client.domain
 {
     public class CheckInstantACHResponse : BaseResponse
     {
+        /// <summary>
+        /// String field used in the BaseResponse object to save reference
+        /// </summary>
+        [JsonProperty("maximum_amount")]
+        public string MaximumAmount { get; set; }
 
     }
 }
